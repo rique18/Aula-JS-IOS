be426de56b21fa5ef46663d53423ff4f7102a9bf
henrique <lucicmaciel@gmail.com>
2026-05-12 09:52:41 -0300

    aula 1pt2

---
+++ Aula 1/tipos_de_dados.js
@@ -0,0 +1,28 @@
+//  "henrique" | 'henrique' | `henrique`
+// 7 | 7.0 | number
+//  true | false | booleano
+
+console.log(`Henrique Cavalheiro`)
+console.log(`nota do aluno foi: ` + 8);
+console.log(6 >= 7);
+
+//  const | Não pode ter seu valor redefinido
+//  let | não pode ser redeclarado, mas pode ser redefinido
+//  var | pode ser redeclarado e pode ter seu valor alterado
+
+const CPF = 123456
+console.log(CPF);
+
+let nome = "Henrique"
+console.log(nome);
+
+nome = "stuart"
+console.log(nome);
+
+var telefone = 40028922
+console.log(telefone)
+var telefone = 234578
+console.log(telefone)
+
+let email_aluno = "stuart.cavalheiro@percorre.org.br"
+let emailAluno = "stuart.cavalheiro@percorre.org.br"
