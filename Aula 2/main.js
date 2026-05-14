51bbc94ff172acbb32319469b819942e76054bcf
henrique <lucicmaciel@gmail.com>
2026-05-14 09:57:15 -0300

    aula2

---
+++ Aula 2/main.js
@@ -0,0 +1,9 @@
+let NomeUsuario = 'gabriel'
+const CPF = 60194873
+// tipos de dados
+let curso = "dev, web" // 'dev, web' | `dev, web` 
+let RA = 123 // 1 e 1.0 tipo number
+let matricula = true // tipo booleano | verdadeiro ou falso
+
+console.log(`O ${NomeUsuario} está cursando ${curso} e tem o R.A: ${RA}`) //mais facil e rapido
+console.log('O ' + NomeUsuario + ' está cursando ' + curso +' e tem o R.A: ' + RA) //mais demorado e complicado
---
+++ Aula 2/operadores.js
@@ -0,0 +1,41 @@
+//var NomeAluno = "stuart"
+//var nota1 = 7
+//var nota2 = 9
+//var media = (nota1 + nota2) /2
+
+//console.log(`O ${NomeAluno} tirou ${nota1} e depois tirou ${nota2} e tirou ${media} de media`)
+
+//let n1 = 7
+//let n2 = 7
+// let res 
+// === se é do mesmo tipo ou n
+// == se tem o mesmo valor ou n
+// = se é igual ou n
+// != se é diferente ou n
+// < se é menor
+// <= se é menor ou igual
+// >=se é maior ou igual
+// > se é maior ou n
+// console.log(n1 === n2)
+
+//let dinheiros = 5000
+//let clima = "sol"
+
+//let res = dinheiros >= 4500 && clima == "sol" ? "partiu cidrerere" : "vou ficar em casa"
+
+// console.log(res)
+
+
+
+let nome = "henrique"
+let nota1 = 2
+let nota2 = 7
+let nota3 = 5
+let nota4 = 10
+let media = (nota1 + nota2 + nota3 + nota4) /4
+ 
+let res = media >= 7 ? "aprovado" : "reprovado"
+
+console.log(res)
+
+
