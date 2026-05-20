56d93adf551706d8cd04b3c9c4f595141efaebd2
henrique <lucicmaciel@gmail.com>
2026-05-20 09:59:06 -0300

    aula3

---
+++ Aula 3/alert.js
@@ -0,0 +1,7 @@
+alert(`Olá, mundo!`)
+
+let n1 = prompt(`digite um numero:`)
+let n2 = prompt(`digite outro numero:`)
+// let res = n1 + n2 
+let res = Number(n1) + Number(n2)
+alert(`O valor da soma é : ${res}`)2
---
+++ Aula 3/main.js
@@ -0,0 +1,21 @@
+// let media = 7.87563
+// console.log(`A media do Aluno é: ${media.toFixed(0)}`)
+
+let curso = "Instituto Percorre"
+let nome = "Stuart"
+
+console.log(curso.charAt(4)) //busca elemneto especifico
+console.log(nome.length) //tamanho da string
+console.log(typeof nome) //tipo de dado
+
+console.log(curso.toUpperCase()) //deixar texto em maiusculo
+console.log(curso.toLowerCase()) //deixar texto em minisculo
+console.log(curso.substring(10))
+console.log(curso.substring(13,18))
+
+let novo_nome = nome.split(' ')
+console.log(novo_nome)
+
+let nome2 = nome.replace('Stuart','henrique')
+
+console.log(nome2);
